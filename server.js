5d065281134c84920732629b181535ab5f52afb1
Farek08 <farkaszsombor08@gmail.com>
2023-11-20 14:10:03 +0100

    A kézpésekhez tartozó útvonalak meghatározása

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 const express = require("express")
 require("dotenv").config()  // A .env fájlt olvassa
+const trainings = require("./routes/trainings")
+
 const app = express()
+
+app.use("/api/trainings", trainings)
+
 app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`))
