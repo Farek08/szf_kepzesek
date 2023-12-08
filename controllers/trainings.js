cc60ae4de650a9e2b67ea1380749cdfd39313ebf
Farek08 <farkaszsombor08@gmail.com>
2023-12-08 13:01:16 +0100

    Kérések fejlesztése

--- controllers/trainings.js
+++ controllers/trainings.js
@@ -5,8 +5,67 @@ const Training = require("../models/Training")
 // @access Public
 exports.getTrainings = async (req, res, next) => {
     try {
-        const trainings = await Training.find()
-        res.status(200).json({ success: true, count: trainings.length, data: trainings });
+        let query;
+        let queryStr = JSON.stringify(req.query)
+        // Kicseréljük a query-ben lévő lte sztringet $lte-re
+        queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`)
+
+        query = Training.find(JSON.parse(queryStr));
+
+        if (req.query.select) {
+            const fields = req.query.select.split(',').join(' ')
+            query = query.select(fields)
+        }
+
+        if (req.query.sort) {
+            const sortBy = req.query.sort.split(',').join(' ')
+            query = query.sort(sortBy)
+        } else {
+            query = query.sort('-createdAt') // Dátum szerint csökkenő rendezés
+        }
+
+
+        const page = parseInt(req.query.page) || 1
+        const limit = parseInt(req.query.page) || 2
+        const startIndex = (page - 1) * limit
+        const endIndex = (page + 1) * limit
+        const total = 5
+        query = query.skip(startIndex).limit(limit)
+
+        const pagination = {};
+        if (startIndex > 0) {
+            pagination.prev = {
+                page: page - 1,
+                limit,
+            };
+        }
+        if (endIndex < total) {
+            pagination.next = {
+                page: page + 1,
+                limit,
+            };
+        }
+        const trainings = await query;
+        res
+            .status(200)
+            .json({
+                success: true,
+                count: trainings.length,
+                pagination,
+                data: trainings,
+            });
+
+
+
+
+        // const trainings = await query;
+
+
+
+
+
+        //const trainings = await Training.find(req.query)
+        // res.status(200).json({ success: true, count: trainings.length, data: trainings });
     } catch (error) {
         res.status(400).json({ success: false });
     }
